933a6cd0d4ed9bd8da509887bd0ccd1e57e40931
Sebastien Caunes <sebastien@pixseed.com>
2015-02-26 14:27:47 +0100

    V 0.9 Works but no check of any kind for errors.

---
+++ DirectAdminCreateSites/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace DirectAdminCreateSites
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxPassWord = new System.Windows.Forms.TextBox();
+            this.textBoxSites = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(47, 444);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(220, 46);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect and create sites";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(47, 58);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(220, 20);
+            this.textBoxUser.TabIndex = 2;
+            // 
+            // textBoxPassWord
+            // 
+            this.textBoxPassWord.Location = new System.Drawing.Point(47, 104);
+            this.textBoxPassWord.Name = "textBoxPassWord";
+            this.textBoxPassWord.Size = new System.Drawing.Size(220, 20);
+            this.textBoxPassWord.TabIndex = 2;
+            // 
+            // textBoxSites
+            // 
+            this.textBoxSites.Location = new System.Drawing.Point(47, 147);
+            this.textBoxSites.Multiline = true;
+            this.textBoxSites.Name = "textBoxSites";
+            this.textBoxSites.Size = new System.Drawing.Size(220, 289);
+            this.textBoxSites.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Websites (one per line)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 147);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 289);
+            this.textBox1.TabIndex = 4;
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(332, 131);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(39, 13);
+            this.labelOutput.TabIndex = 3;
+            this.labelOutput.Text = "Output";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(45, 19);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(220, 20);
+            this.textBoxURL.TabIndex = 2;
+            this.textBoxURL.Text = "http://server:2222";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "http://Server:port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 502);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSites);
+            this.Controls.Add(this.textBoxPassWord);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "DirectAdmin Bulk Website adder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxPassWord;
+        private System.Windows.Forms.TextBox textBoxSites;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DirectAdminCreateSites/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.Remoting;
+using System.Windows.Forms;
+
+namespace DirectAdminCreateSites
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreateWebsites_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            using (var client = new CookieAwareWebClient())
+            {
+                var values = new NameValueCollection
+                             {
+                                 {"username", textBoxUser.Text},
+                                 {"password", textBoxPassWord.Text},
+                             };
+                //client.UploadValues(textBoxURL.Text + "/CMD_LOGIN", values);
+                client.UploadValues(textBoxURL.Text + "/CMD_LOGIN", values);
+
+
+                string[] sites = textBoxSites.Text.Replace("\r\n", "#").Split('#');
+                foreach (string site in sites)
+                {
+                    var values2 = new NameValueCollection
+                                  {
+                                      {"action", "create"},
+                                      {"domain", site},
+                                      {"ubandwidth", "unlimited"},
+                                      {"uquota", "unlimited"},
+                                      {"cgi", "OFF"},
+                                      {"php", "ON"},
+                                  };
+                    //client.UploadValues(textBoxURL.Text + "/CMD_LOGIN", values);
+                    client.UploadValues("http://37.59.47.46:2222" + "/CMD_DOMAIN", values2);
+                    textBox1.AppendText("Domain " + site + " added\r\n");
+                }
+            }
+        }
+    }
+}
